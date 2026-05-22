93b7c2fb3c5c933538efe56fc02bce2a7cdb3361
Neil Chen <neilchen1998@gmail.com>
2026-05-21 19:45:26 -0500

    renamed to parse_sudoku_board and added test cases

---
+++ tests/test_parse_sudoku_board.py
@@ -0,0 +1,117 @@
+import os
+import pytest
+
+from pathlib import Path
+
+from parse_sudoku_board import is_valid_sudoku
+
+# This is a valid and completed Sudoku board
+VALID_COMPLETED = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]
+
+# This is a valid Sudoku in which all cells are 0
+VALID_EMPTY = [[0] * 9 for _ in range(9)]
+
+# This is a valid Sudoku
+VALID_PARTIAL = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+# This is an invalid Sudoku
+# There are two 5's in the first row
+INVALID_ROW = [
+    [5, 5, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+# This is an invalid Sudoku
+# There are two 6's in the left-most column
+INVALID_COL = [
+    [6, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+# This is an invalid Sudoku
+# There are two 1's in the top-left grid
+INVALID_SUBGRID = [
+    [5, 3, 1, 0, 7, 0, 0, 0, 0],
+    [6, 1, 0, 0, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9],
+]
+
+@pytest.mark.parametrize(
+    "board",
+    [
+        VALID_COMPLETED,
+        VALID_EMPTY,
+        VALID_PARTIAL,
+    ],
+    ids=[
+        "valid_completed",
+        "valid_empty",
+        "valid_partial",
+    ]
+)
+def test_valid_boards(board):
+    """
+    Verifies that all structurally correct Sudoku boards return True
+    """
+
+    assert is_valid_sudoku(board) is True
+
+@pytest.mark.parametrize(
+    "board",
+    [
+        INVALID_ROW,
+        INVALID_COL,
+        INVALID_SUBGRID
+    ],
+    ids=[
+        "duplicate_in_row",
+        "duplicate_in_col",
+        "duplicate_in_subgrid"
+    ]
+)
+def test_invalid_boards(board):
+    """
+    Verifies that all structurally incorrect Sudoku boards return False
+    """
+
+    assert is_valid_sudoku(board) is False
