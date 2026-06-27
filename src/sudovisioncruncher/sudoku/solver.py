11da13dd1c7a8f0c1f5b27d029401adda3f4c113
Neil Chen <neilchen1998@gmail.com>
2026-06-27 10:08:10 -0500

    refactored solve_sudoku into SudokuMRVSolver

--- src/sudovisioncruncher/sudoku/solver.py
+++ src/sudovisioncruncher/sudoku/solver.py
@@ -9,13 +9,54 @@ def solve_sudoku(board: list[list[int]]) -> list[list[int]]:
         list[list[int]]: The solved Sudoku board
     """
 
-    # Bitmasks for constraints
-    rows = [0] * 9
-    cols = [0] * 9
-    boxes = [0] * 9
-    empties = []
+    return SudokuMRVSolver(board).solve()
 
-    def box_index(r: int, c: int) -> int:
+
+class SudokuMRVSolver:
+
+
+    def __init__(self, board: list[list[int]]):
+
+        self.board = board
+
+        # Bitmasks for constraints
+        self.rows = [0] * 9
+        self.cols = [0] * 9
+        self.boxes = [0] * 9
+        self.empties: list[tuple[int, int]] = []
+
+        # Initialize the constraints
+        for r in range(9):
+            for c in range(9):
+
+                num = self.board[r][c]
+
+                if num == 0:
+                    self.empties.append((r, c))
+                    continue
+
+                # Convert the digit to bit
+                bit = 1 << num
+
+                # Store it in the constraints
+                self.rows[r] |= bit
+                self.cols[c] |= bit
+                self.boxes[self._box_index(r, c)] |= bit
+
+    def solve(self) -> list[list[int]]:
+        """
+        Solves the Sudoku given puzzle.
+
+        Returns:
+            list[list[int]]: The solved puzzle
+        """
+
+        self._backtrack()
+
+        return self.board
+
+    @staticmethod
+    def _box_index(r: int, c: int) -> int:
         """
         Calculates the box index based on the cell.
 
@@ -29,67 +70,70 @@ def solve_sudoku(board: list[list[int]]) -> list[list[int]]:
 
         return (r // 3) * 3 + (c // 3)
 
-    # Initialize masks
-    for r in range(9):
-        for c in range(9):
-            if board[r][c] == 0:
-                empties.append((r, c))
-            else:
-                bit = 1 << board[r][c]
-                rows[r] |= bit
-                cols[c] |= bit
-                boxes[box_index(r, c)] |= bit
-
-    def backtrack(i: int = 0) -> bool:
+    def _find_best_cell(self) -> tuple[int, int]:
         """
-        Recursively solves the Sudoku puzzle using backtracking
-
-        Args:
-            i: The index of the empty list
+        Finds the emtpy cell with the fewest candidates.
 
         Returns:
-            bool: True if the Sudoku is solved successfully, False if no valid number can be placed
+            tuple:
+                int: index of the cell with the fewest candidates
+                int: the mask of its candidates
         """
 
         best_idx = -1
-        best_candidate = 0
+        best_candidates = 0
         best_cnt = 10
 
         # Loop through all emtpy cells to find the cell that ahs the least amount of candidates
-        for i, (r, c) in enumerate(empties):
+        for i, (r, c) in enumerate(self.empties):
 
             # Skip non-empty cells
-            if board[r][c] != 0:
+            if self.board[r][c] != 0:
                 continue
 
-            b = box_index(r, c)
+            b = self._box_index(r, c)
 
             # Get all the candidates in bit-format
-            candidates = (~(rows[r] | cols[c] | boxes[b])) & 0x3FE
+            candidates = (~(self.rows[r] | self.cols[c] | self.boxes[b])) & 0x3FE
             cnt = candidates.bit_count()
 
             # This is a dead end since it is impossible to have a cell w/o candidate
             if cnt == 0:
-                return False
+                return -2, 0
 
             # Update the current best candidate
             if cnt < best_cnt:
                 best_cnt = cnt
-                best_candidate = candidates
+                best_candidates = candidates
                 best_idx = i
 
                 # Early break since this is the best candidate that we can get
                 if cnt == 1:
                     break
 
+        return best_idx, best_candidates
+
+    def _backtrack(self) -> bool:
+        """
+        Recursively solves the Sudoku puzzle using backtracking.
+
+        Returns:
+            bool: True if the Sudoku is solved successfully, False if no valid number can be placed
+        """
+
+        idx, candidates = self._find_best_cell()
+
         # Early return if there is no non-empty cell
-        if best_idx == -1:
+        if idx == -1:
             return True
 
+        # Early return if there is a dead end
+        elif idx == -2:
+            return False
+
         # Get the metadata of the best cell
-        r, c = empties[best_idx]
-        b = box_index(r, c)
-        candidates = best_candidate
+        r, c = self.empties[idx]
+        b = self._box_index(r, c)
 
         # Loop through all the possible candidates
         while candidates:
@@ -99,24 +143,20 @@ def solve_sudoku(board: list[list[int]]) -> list[list[int]]:
             num = bit.bit_length() - 1
 
             # Place the digit
-            board[r][c] = num
-            rows[r] |= bit
-            cols[c] |= bit
-            boxes[b] |= bit
+            self.board[r][c] = num
+            self.rows[r] |= bit
+            self.cols[c] |= bit
+            self.boxes[b] |= bit
 
             # Proceed to find next cell
-            if backtrack():
+            if self._backtrack():
                 return True
 
             # Undo
-            board[r][c] = 0
-            rows[r] ^= bit
-            cols[c] ^= bit
-            boxes[b] ^= bit
+            self.board[r][c] = 0
+            self.rows[r] ^= bit
+            self.cols[c] ^= bit
+            self.boxes[b] ^= bit
             candidates &= (candidates - 1)
 
         return False
-
-    backtrack()
-
-    return board
